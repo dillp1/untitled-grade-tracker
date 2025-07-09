2f63331c2d286b72e8d85c2638ec62ea06095f27
Dillon Michlena <dillon.michlena@gmail.com>
2025-07-09 12:29:59 -0400

    slight restart

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
